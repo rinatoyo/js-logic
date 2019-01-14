d2f6175e13f7f34762994d79f3832631248187b7
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-01-13 14:57:11 -1000

    number 9

--- exercises.js
+++ exercises.js
@@ -200,7 +200,16 @@ console.log('spareChange:', spareChange(150));
  * Console.log your result.
 */ 
 
+function dirty30(one, two, three){
+  var sum = one + two + three;
+  if (sum > 30){
+    return true;
+  }else{
+    return false;
+  }
+}
 
+console.log('dirty30:', dirty30(10, 7, 15));
 
 /*
  * #10
