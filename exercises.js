e95a196ac12846050cb06f3ea1893bc956d2e1f6
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-01-14 19:57:03 -1000

    number 16

--- exercises.js
+++ exercises.js
@@ -414,6 +414,10 @@ for (var i = 0; i<players.length; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+  for (var i = 0; i<myFavFoods.length; i++){
+    console.log('myFavFoods:', myFavFoods[i]);
+  }
+
 
 /*
  * #17
